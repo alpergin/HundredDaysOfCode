0c10ec1f66046049ed5b20dd58b5cd56e2a4993c
alper ergin <ergin.sdet@gmail.com>
2021-10-17 00:45:25 +0300

    input and lst manipÄul length added

---
+++ .Classes/Input.py
@@ -0,0 +1,8 @@
+# #input() will get user input in console
+# Then print() will print the word "Hello" and the user input
+# print ("Hello " + input("What is your name?"))
+#
+
+print("Hello " + input("What is your name?"))
+
+print(input())
---
+++ .Classes/StringManipulationLength.py
@@ -0,0 +1,3 @@
+#Write your code below this line 👇
+print(len("Angela"))
+# it will gives 6
