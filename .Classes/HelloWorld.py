b990fa03c59fafd14b8202a369f2ae321705ed3f
alper ergin <ergin.sdet@gmail.com>
2021-10-16 23:53:21 +0300

    string concat

--- .Classes/HelloWorld.py
+++ .Classes/HelloWorld.py
@@ -1,1 +1,13 @@
 print("Hello Fuckin' World")
+
+#string concat
+
+print("My name " + "is" + " " + "Alper")
+
+#target
+
+print("I will be Devops Engineer after 6 months")
+
+#this is comment line
+
+
